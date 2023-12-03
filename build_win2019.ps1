a40e6a8c222565b0fc2b548faf5ab1cae8349b3c
tom <tom@163.com>
2023-12-03 10:47:30 +0800

    update fix

--- build_win2019.ps1
+++ build_win2019.ps1
@@ -4,7 +4,7 @@ packer build --only=hyperv-iso `
       --var iso_checksum=a876d230944abe3bf2b5c2b40da6c4a3 `
       --var autounattend=./answer_files/2019/Autounattend.xml `
       --var hyperv_switchname=nat-switch `
-      ws2019v1.json
+      ws2019.json
 
 
 packer build --only=virtualbox-iso `
@@ -12,4 +12,4 @@ packer build --only=virtualbox-iso `
       --var iso_url=d:\\iso\\en_win2019.iso `
       --var iso_checksum=a876d230944abe3bf2b5c2b40da6c4a3 `
       --var autounattend=./answer_files/2019/Autounattend.xml
-      ws2019v1.json
+      ws2019.json
